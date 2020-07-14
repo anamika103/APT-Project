54a5c94e5f9cd906d2bd91814f540211c63860e0
dimpal103 <37858836+dimpal103@users.noreply.github.com>
2020-07-14 17:58:06 +0530

    1st commit ....hello world

---
+++ apiTesting/src/main/java/API_ALL_Code/apiTesting/App.java
@@ -0,0 +1,13 @@
+package API_ALL_Code.apiTesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
